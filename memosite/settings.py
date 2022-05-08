321ff8e2b9b91e96bf4162060a5be4de555d36ea
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-08 13:13:32 +0900

    debug=false

--- memosite/settings.py
+++ memosite/settings.py
@@ -23,7 +23,7 @@ except ImportError:
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
