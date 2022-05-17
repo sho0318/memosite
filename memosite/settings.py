8640a9d2bbe1114f3630540e18d48ed3b2ec692c
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-17 16:25:30 +0900

    add whitenoise

--- memosite/settings.py
+++ memosite/settings.py
@@ -22,6 +22,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 if not DEBUG:
