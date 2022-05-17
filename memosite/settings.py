006156e4fbae1dc9907f18e866eba501c52f8d82
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-17 16:02:10 +0900

    static

--- memosite/settings.py
+++ memosite/settings.py
@@ -39,7 +39,7 @@ if not DEBUG:
 
     # Static files (CSS, JavaScript, Images)
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATIC_URL = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = "/staticfiles/"
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'micromemo/static'),
