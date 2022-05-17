93c4b6fd18f28c5af373aa123a39897afe83f396
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-17 15:50:34 +0900

    static setting

--- memosite/settings.py
+++ memosite/settings.py
@@ -13,15 +13,6 @@ except ImportError:
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
