40553d1c4bbdbffd488319c39d829d51604e560a
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-17 14:36:54 +0900

    change setting

--- memosite/settings.py
+++ memosite/settings.py
@@ -52,7 +52,7 @@ if not DEBUG:
     # Static files (CSS, JavaScript, Images)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
-
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'micromemo/static'),
