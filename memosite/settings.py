e6eb0dc71b7829928eee131a1c6986cfe375baa3
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-19 12:32:49 +0900

    db

--- memosite/settings.py
+++ memosite/settings.py
@@ -166,12 +166,20 @@ EMAIL_USE_TLS = True
 if not DEBUG:
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
-
-db_from_env = dj_database_url.config()
-DATABASES = {
-    'default': dj_database_url.config()
-}
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
