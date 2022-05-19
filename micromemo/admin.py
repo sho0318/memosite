bc8bf862072543c05f13c967c5f6ac6dcb9c4055
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-19 16:46:53 +0900

    admin

--- micromemo/admin.py
+++ micromemo/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Document,Directory
 
 # Register your models here.
-admin.site.register(Document)
-admin.site.register(Directory)
+class DirectoryAdmin(admin.ModelAdmin):
+    fields = ['create_user', 'title', 'comment', 'posted_date', 'id']
+
+class DocumentAdmin(admin.ModelAdmin):
+    fields = ['post','title','url','comment','posted_date','id']
+
+admin.site.register(Document,DocumentAdmin)
+admin.site.register(Directory,DirectoryAdmin)
