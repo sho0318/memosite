ca90d1f4cfd5206b521ca5947ce2c4b755525d50
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-19 16:51:48 +0900

    admin

--- micromemo/admin.py
+++ micromemo/admin.py
@@ -3,10 +3,10 @@ from .models import Document,Directory
 
 # Register your models here.
 class DirectoryAdmin(admin.ModelAdmin):
-    fields = ['create_user', 'title', 'comment', 'posted_date', 'id']
+    fields = ['create_user', 'title', 'comment']
 
 class DocumentAdmin(admin.ModelAdmin):
-    fields = ['post','title','url','comment','posted_date','id']
+    fields = ['post','title','url','comment']
 
 admin.site.register(Document,DocumentAdmin)
 admin.site.register(Directory,DirectoryAdmin)
