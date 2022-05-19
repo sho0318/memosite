418de3687325cd72440664fb0a7ee20f3599cde0
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-19 11:25:00 +0900

    appname

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from .views import SignUpView,HomeView
 
+app_name = 'memosite'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
